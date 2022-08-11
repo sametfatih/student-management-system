1431455b745e2830f02ed2caef60697076865b3a
samet fatih <samet.fatih.gozum@hotmail.com>
2022-08-11 23:54:48 +0300

    Initial commit

--- src/main/java/com/sametfatih/studentmanagment/system/dataAccess/abstracts/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sametfatih.studentmanagment.system.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sametfatih.studentmanagment.system.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
